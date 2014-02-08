fb090e017928035fef80d14fa6f04e06ef499fab
Marco Hutzsch <marco@hutzsch.de>
2014-02-08 14:49:18 +0100

    added test for creating user

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe 'graylog2server' do
-  let (:params) { { :path => '/opt/graylog' } }
+  let (:params) { 
+    { :path => '/opt/graylog', 
+      :user => 'graylog2'} }
 
   it "creates the graylog directory" do
     should contain_file('/opt/graylog').with(
       'ensure' => 'directory')
+  end
+
+  it "creates the graylog2 user" do
+    should contain_user('graylog2').with(
+      'ensure' => 'present',
+      'home'   => '/opt/graylog')
   end
 end
