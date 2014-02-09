7976819825cb65b21cd195ad5df4064e0f74b290
Marco Hutzsch <marco@hutzsch.de>
2014-02-09 14:37:32 +0100

    make use of service and user

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,38 +7,76 @@
 #
 #
 ### Params ###
-# path = full path where graylog2 shall be installed
-# user = username for graylog2 user
-# uid = userid for graylog2 user
-# gid = groupid for graylog2 main user group
-# run = start graylog2 using runit
-# version = version of graylog2server
+# graylog2serverpath = full graylog2serverpath where graylog2 shall be installed
+# user     = username for graylog2 user
+# uid      = userid for graylog2 user
+# gid      = groupid for graylog2 main user group
+# run      = start graylog2 using runit
+# version  = version of graylog2server
+# logpath  = where to store logiles when running graylog2-server
+# mongodb  = shall a mongodb instance be installed on node
 
 class graylog2server (
-  $path = '/opt/gralog2',
-  $user = 'graylog2',
-  $uid = 1042,
-  $gid = 1042,
-  $run = true,
-  $version = '0.20.0-rc.1-1') {
+  $graylog2serverpath = '/opt/graylog2-server',
+  $user               = 'graylog2',
+  $uid                = 1042,
+  $run                = true,
+  $version            = '0.20.0-rc.1-1',
+  $logpath            = '/var/log/graylog2server',
+  $mongodb            = true) {
 
-  file { $path:
+  if ($mongodb == true) {
+    include graylog2server::mongodb
+  }
+
+  include graylog2server::service
+
+  # create the user
+  graylog2server::user { $user:
+    user => $user,
+    home => $graylog2serverpath,
+    uid  => $uid,
+  }
+
+  file { $graylog2serverpath:
     ensure  => directory,
     owner   => $user,
+    group   => $user,
     mode    => '0644',
+    recurse => true,
     require => User[$user]
   }
 
-  group { $user:
-    ensure => present,
+  file { "${graylog2serverpath}/etc":
+    ensure  => directory,
+    owner   => $user,
+    group   => $user,
+    mode    => '0644',
+    require => File[$graylog2serverpath]
   }
 
-  user { $user:
-    ensure  => present,
-    home    => $path,
-    uid     => $uid,
-    gid     => $gid,
-    require => Group[$user]
+  # get and unpack graylog2
+  exec { 'download and unpack graylog2-server':
+    command => "wget https://github.com/Graylog2/graylog2-server/releases/download/${version}/graylog2-server-${version}.tgz -O ${graylog2serverpath}/package.tgz;
+      tar xvfz package.tgz;
+      rm -rf package.tgz;
+      mv graylog2-server-${version}/* ${graylog2serverpath};
+      rm -rf graylog2-server-${version}",
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    cwd     => $graylog2serverpath,
+    user    => $user,
+    group   => $::group,
+    unless  => "test -f ${graylog2serverpath}/graylog2-server.jar"
+  }
+
+  # create basic graylog2 config file
+  file { "${graylog2serverpath}/etc/graylog2.conf":
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'graylog2',
+    group   => 'graylog2',
+    content => template('graylog2server/graylog2.conf.erb'),
+    require => File["${graylog2serverpath}/etc"]
   }
 
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,18 +1,16 @@
 require 'spec_helper'
 
 describe 'graylog2server' do
+  
   let (:params) { 
-    { :path => '/opt/graylog', 
-      :user => 'graylog2'} }
+    { :graylog2serverpath => '/opt/graylog', 
+      :mongodb            => 'false' }
+  }
+
+  let(:facts) { { :osfamily  => 'Debian' } }
 
   it "creates the graylog directory" do
     should contain_file('/opt/graylog').with(
       'ensure' => 'directory')
-  end
-
-  it "creates the graylog2 user" do
-    should contain_user('graylog2').with(
-      'ensure' => 'present',
-      'home'   => '/opt/graylog')
   end
 end
